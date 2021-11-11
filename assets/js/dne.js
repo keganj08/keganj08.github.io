a2a19bd1741359d0bf35ebf5a7c1c9c98690c473
Kegan Johnson <keganj08@gmail.com>
2021-11-11 14:26:24 -0600

    Changing dne message

--- assets/js/dne.js
+++ assets/js/dne.js
@@ -4,12 +4,12 @@ msgs = [
     "Somebody hasn't coded me yet. You should peer pressure them.",
     "I hope to be a real boy some day, but for now I'm just an unfinished web page.",
     "I won't turn back into a finished web page until you kiss me.",
-    "It's rude to walk in on someone while they're compiling.",
     "&lt;website&gt;&lt;/website&gt;. Dang, that didn't work.",
     "I would say my biggest role model is Ultron. I think that's why Kegan hasn't coded me.",
     "How unfair that you got to be human and I'm just some pixels waiting to be worked on.",
     "If you were an unfinished website, how would you pass the time?",
-    "This website is fully functional, you're only dreaming that this is a mostly blank screen."
+    "This website is fully functional, you're only dreaming that this is a mostly blank screen.",
+    "This is the game. Are you having fun?"
 ];
 
 $(document).ready(function(){
